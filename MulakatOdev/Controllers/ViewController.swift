0e55dd9bf04bf1b4cd42447b467bc58db83ee1ef
keremkiyak <110934008+keremkiyak@users.noreply.github.com>
2023-04-04 19:20:49 +0300

    Add files via upload
    I solved the problem that the return button in the other view controller was removed due to the navigation bar being hidden

--- MulakatOdev/Controllers/ViewController.swift
+++ MulakatOdev/Controllers/ViewController.swift
@@ -13,10 +13,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     @IBOutlet weak var tableView: UITableView!
-//MARK: - Table view hücrelerinde yazdıracağımız metinler dizisi
+    //MARK: - Table view hücrelerinde yazdıracağımız metinler dizisi
     let brandArray = ["Audi", "Bmw", "Chrysler", "Fiat", "Honda", "Jaguar", "Lexus", "Mini", "Porsche", "Volkswagen", "Volvo"]
     
-//MARK: - Table view hücrelerinde yazdıracağımız image ların isimlerinin dizisi
+    //MARK: - Table view hücrelerinde yazdıracağımız image ların isimlerinin dizisi
     let brandImages = ["audi", "bmw", "chrysler", "fiat", "honda", "jaguar", "lexus", "mini", "porsche", "volkswagen", "volvo"]
     
     override func viewDidLoad() {
@@ -24,29 +24,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         tableView.showsVerticalScrollIndicator = true
+        
     }
     
-//MARK: - Bu fonksiyon sayesinde scroll view um en üste geldiğinde navigation bar ımın gözükmemesini sağladım.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.hidesBackButton = false
+    }
+    
+    
+    
+    //MARK: - Bu fonksiyon sayesinde scroll view um en üste geldiğinde navigation bar ımın gözükmemesini sağladım.
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-            if scrollView.contentOffset.y <= 0 {
-                navigationController?.setNavigationBarHidden(true, animated: true)
-            } else {
-                navigationController?.setNavigationBarHidden(false, animated: true)
-            }
+        if scrollView.contentOffset.y <= 0 {
+            navigationController?.setNavigationBarHidden(true, animated: true)
+        } else {
+            navigationController?.setNavigationBarHidden(false, animated: true)
         }
-
+        
+    }
     
     
-//    MARK: - Table view un genel iki fonksiyonu
+    
+    //    MARK: - Table viewa verileri yazdırmamı sağlayacak  fonksiyonlar
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return brandArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-     
-
+        
+        
         cell.textLabel?.text = brandArray[indexPath.row]
         cell.imageView?.image = UIImage(named: brandImages[indexPath.row])
         
@@ -62,38 +71,55 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return 100.0
     }
     
-//    MARK: - Burada table view umuza "Markalar" başlığını ekledik
+    //    MARK: - Burada table view umuza "Markalar" başlığını ekledik
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-            let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 50))
-            headerView.backgroundColor = .systemGray6
-
-            let titleLabel = UILabel(frame: CGRect(x: 15, y: 0, width: headerView.frame.width - 30, height: headerView.frame.height))
-            titleLabel.font = UIFont.boldSystemFont(ofSize: 30)
-            titleLabel.text = "Markalar"
-
-            headerView.addSubview(titleLabel)
-
-            return headerView
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 50))
+        headerView.backgroundColor = .systemGray6
+        
+        let titleLabel = UILabel(frame: CGRect(x: 15, y: 0, width: headerView.frame.width - 30, height: headerView.frame.height))
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        titleLabel.text = "Markalar"
+        
+        headerView.addSubview(titleLabel)
+        
+        return headerView
+    }
+    
+//    header ın yüksekliğini ayarladım.
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    
+    
+//    navigation bar ı gizleyince segue ile geçtiğimiz sayfadaki table view a geri dönme butonunu kaybetmemek için segue ile diğer view controller a geçerken geri dönmemizi sağlayacak butonu bu kodla geri getirdim.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toAudi" ||
+            segue.identifier == "toBmw"  ||
+            segue.identifier == "toChrysler" ||
+            segue.identifier == "toFiat" ||
+            segue.identifier == "toHonda" ||
+            segue.identifier == "toJaguar" ||
+            segue.identifier == "toLexus" ||
+            segue.identifier == "toMini" ||
+            segue.identifier == "toPorsche" ||
+            segue.identifier == "toVolkswagen"
+        {
+            navigationController?.setNavigationBarHidden(false, animated: true)
+            }
         }
-
-        func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-            return 50
-        }
-
-  
-   
+    
 
 
 
-
-
-//MARK: - Bu fonksiyonda table view un her bir hücresi tıklanması için ayrı bir segue ile ayrı bir ekrana yönlendirme yaptık.
+//MARK: - Bu fonksiyonda table view un her bir hücresi tıklanması için ayrı bir segue ile ayrı bir ekrana yönlendirme yaptım.
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selectedBrand = brandArray[indexPath.row]
         tableView.deselectRow(at: indexPath, animated: true)
         switch selectedBrand {
         case "Audi":
             performSegue(withIdentifier: "toAudi", sender: nil)
+            
         case "Bmw":
             performSegue(withIdentifier: "toBmw", sender: nil)
         case "Chrysler":
